4be5f6f020c6ae1aa53d87f0387b5b174a4a82cc
Dylan Guldy <dylanguldy@Dylans-MacBook-Air.local>
2026-01-04 11:12:59 -0500

    moving back over to my pc

--- physics/line.py
+++ physics/line.py
@@ -7,13 +7,17 @@ class Line(GameObject):
         super().__init__()
         self.has_gravity = False
         self.normal = Vector2()
+        self.start_point = (0,0)
+        self.end_point = (0,0)
 
-    def __init__(self, x_location, y_location, pixel_width):
+    def __init__(self, start_point, end_point, pixel_width):
         super().__init__()
         self.has_gravity = False
-        self.position.x = x_location
-        self.position.y = y_location
+        self.start_point = start_point
+        self.end_point = end_point
+        self.normal.x = -1*(end_point[0]- start_point[0])
+        self.normal.y = end_point[1] - start_point[1]
         self.pixel_width = pixel_width
 
     def draw(self, surface:pygame.Surface):
-        pygame.draw.line(surface, (0xff, 0xff, 0xff), self.position.x, self.position.y, self.pixel_width)
+        pygame.draw.line(surface, (0xff, 0xff, 0xff), self.start_point, self.end_point, self.pixel_width)
